373b588eb631bafd3b88183a547ac298650258f5
Steve McLoughlin <steve.mcloughlin@hotmail.co.uk>
2021-06-07 10:17:55 +0100

    add mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { routes } from './routes/index.js';
+import { db } from './db/index.js';
 
 const app = express();
 
@@ -12,6 +13,11 @@ routes.forEach((route) => {
   app[route.method](route.path, route.handler);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+const start = async () => {
+  await db.connect('mongodb://localhost:27017');
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+};
+
+start();
